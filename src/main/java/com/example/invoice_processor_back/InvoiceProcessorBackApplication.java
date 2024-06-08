d1a93f5dd8a5ee5babba4ff0d9ce7296a58c5fb1
Iván Causera <ivancausera@gmail.com>
2024-06-08 10:18:18 +0200

    Initializr

---
+++ src/main/java/com/example/invoice_processor_back/InvoiceProcessorBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.invoice_processor_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceProcessorBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceProcessorBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/invoice_processor_back/InvoiceProcessorBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invoice_processor_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceProcessorBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
